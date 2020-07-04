7dba2372b0aa690d37e093e99e2f9d68b87d8a89
Jacob Deane <git@jacobdeane.com>
2020-07-04 16:42:56 +0100

    Added test contact form to contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,7 +13,21 @@ class ContactPage extends React.Component {
         />
         <article>
           <h1>Contact</h1>
-          <p>Contact form goes here</p>
+          <p>Please enter your contact details and a short message below and I will try to answer your query as soon as possible.</p>
+          <form name="contact" method="POST" data-netlify="true" netlify>
+            <p>
+              <label htmlFor="name">Your Name: <input type="text" name="name" aria-label="name" /></label>   
+            </p>
+            <p>
+              <label htmlFor="email">Your Email: <input type="email" name="email" aria-label="email" /></label>
+            </p>
+            <p>
+              <label htmlFor="message">Message: <textarea name="message" aria-label="message" ></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send Message</button>
+            </p>
+          </form>
         </article>
       </Layout>
     )
