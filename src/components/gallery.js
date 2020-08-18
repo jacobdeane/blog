dcbfe687b5ddf2e8f17420221a794bc66b97ce24
Jacob Deane <git@jacobdeane.com>
2020-08-18 20:49:28 +0100

    fixed gallery css webpack bug

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { Component } from "react"
 import {Helmet} from 'react-helmet'
 
-import "/js/photoswipe/photoswipe.css"
-import "/js/photoswipe/default-skin/default-skin.css" 
-
-class Gallery extends React.Component {
-  render() {
-
+class Gallery extends Component {
+  render(props) {
     return (
+      <React.Fragment>
         <Helmet>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/photoswipe.css"></link>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/default-skin/default-skin.css"></link>
           <script src="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/photoswipe.min.js"></script>
           <script src="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/photoswipe-ui-default.min.js"></script>
           <script src="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/photoswipe.init.js"></script>
         </Helmet>
+      </React.Fragment>
     )
   }
 }
