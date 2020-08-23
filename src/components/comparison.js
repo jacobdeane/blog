c548841ce3b5cb89996e4754e9488062f6b8424e
Jacob Deane <git@jacobdeane.com>
2020-08-23 16:31:24 +0100

    Stopped juxtapose script from being loaded more than once per page.

--- src/components/comparison.js
+++ src/components/comparison.js
@@ -4,14 +4,18 @@ import './comparison.scss'
 
 export default class Comparison extends Component {
 
-  componentDidMount () {
-      let script = document.createElement("script");
-      let anchor = document.getElementById("page__footer");
-      script.setAttribute("src", "/js/juxtapose/juxtapose.min.js");
-      anchor.appendChild(script);
-  }
+	componentDidMount () {
+		let juxtapose = document.getElementById("juxtapose__script");
+		if(!juxtapose){ //only load this script once!
+			let script = document.createElement("script");
+			let anchor = document.getElementById("page__footer");
+			script.setAttribute("src", "/js/juxtapose/juxtapose.min.js");
+			script.setAttribute("id", "juxtapose__script");
+			anchor.appendChild(script);
+		}
+	}
 
-  render() {
-  	return null
-  }
+	render() {
+		return null
+	}
 }
