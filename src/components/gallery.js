96aaa593acc36a4e3d42b60cb508e3cc5e8b0011
Jacob Deane <git@jacobdeane.com>
2020-08-23 16:52:39 +0100

    Removed cdn workaround to get js loaded in browser

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,20 +1,42 @@
 import React, { Component } from "react"
 import {Helmet} from 'react-helmet'
 
-class Gallery extends Component {
-  render(props) {
+//import './photoswipe.css'
+
+export default class Gallery extends Component {
+
+  componentDidMount () {
+    let photoswipe = document.getElementById("photoswipe__script");
+    if(!photoswipe){ //only load these scripts once!
+      let anchor = document.getElementById("page__footer");
+      
+      let scriptPhotoswipe = document.createElement("script");
+      scriptPhotoswipe.setAttribute("src", "/js/photoswipe/photoswipe.min.js");
+      scriptPhotoswipe.setAttribute("id", "photoswipe__script");
+
+      let scriptPhotoswipeUI = document.createElement("script");
+      scriptPhotoswipeUI.setAttribute("src", "/js/photoswipe/photoswipe-ui-default.min.js");
+
+      let scriptPhotoswipeInit = document.createElement("script");
+      scriptPhotoswipeInit.setAttribute("src", "/js/photoswipe/photoswipe.init.js");
+
+      //we should probably combine the above at some point...
+
+      anchor.appendChild(scriptPhotoswipe);
+      anchor.appendChild(scriptPhotoswipeUI);
+      anchor.appendChild(scriptPhotoswipeInit);
+
+    }
+  }
+
+  render() {
     return (
       <React.Fragment>
         <Helmet>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/photoswipe.css"></link>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/default-skin/default-skin.css"></link>
-          <script src="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/photoswipe.min.js"></script>
-          <script src="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/photoswipe-ui-default.min.js"></script>
-          <script src="https://cdn.jsdelivr.net/gh/jacobdeane/blog/static/js/photoswipe/photoswipe.init.js"></script>
+          <link rel="stylesheet" href="/js/photoswipe/photoswipe.css"></link>
+          <link rel="stylesheet" href="/js/photoswipe/default-skin/default-skin.css"></link>
         </Helmet>
       </React.Fragment>
     )
   }
 }
-
-export default Gallery
