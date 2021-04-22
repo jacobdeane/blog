ba170fc847ba7995489518f7dbe250f414d4d21f
Jacob Deane <git@jacobdeane.com>
2021-04-22 20:49:14 +0100

    tweaked article css to add glassmorphism and swapped to sans-serif font stack

--- src/components/hero.js
+++ src/components/hero.js
@@ -24,21 +24,23 @@ class Hero extends React.Component {
 	  		/>
 	  		<div className='hero__overlay' >
 	  			<div className='hero__row' >
-		  			<div className='hero__text' >
-		  				<ul className='hero__categories'>
-							{categories.map((link) => (
-								<li key={link} className='hero__categories__item' >
-									<Link to={'/' + link}>
-									{link.replace('-',' ')}
-									</Link>
-								</li>
-							))}
-						</ul>
-		  				<h1>{title}</h1>
-		  				<p className='hero__excerpt' >{excerpt}</p>
-		  				<p className='hero__date' >{date}</p>
-		  			</div>
-		  		</div>
+		  			<div className='hero__bg' >
+			  			<div className='hero__text' >
+			  				<ul className='hero__categories'>
+								{categories.map((link) => (
+									<li key={link} className='hero__categories__item' >
+										<Link to={'/' + link}>
+										{link.replace('-',' ')}
+										</Link>
+									</li>
+								))}
+							</ul>
+			  				<h1>{title}</h1>
+			  				<p className='hero__excerpt' >{excerpt}</p>
+			  				<p className='hero__date' >{date}</p>
+			  			</div>
+			  		</div>
+			  	</div>
 	  		</div>
   		</header>
   	)
